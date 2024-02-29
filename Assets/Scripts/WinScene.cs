6e2978592f3e437e5906f98c8f4b0fa4629d8ef3
Elisabet Salazar <40662951+elisa6@users.noreply.github.com>
2024-02-29 14:01:23 -0500

    Revert "Revert "agregar audio""

--- Assets/Scripts/WinScene.cs
+++ Assets/Scripts/WinScene.cs
@@ -8,7 +8,7 @@ public class WinScene : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		Invoke("LoadMainMenu",3);
+		Invoke("LoadMainMenu",10);
 	}
 
 	public void LoadMainMenu()
